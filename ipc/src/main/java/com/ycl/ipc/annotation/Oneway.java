9a1e970dee6a0a2533e110fd2daafc2b7aa2f6c2
yuchenglong12 <yuchenglong12@faw.com.cn>
2024-01-16 16:49:24 +0800

    support unsubscribe

--- ipc/src/main/java/com/ycl/ipc/annotation/Oneway.java
+++ ipc/src/main/java/com/ycl/ipc/annotation/Oneway.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 单项调用
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Oneway {
--- ipc/src/main/java/com/ycl/ipc/annotation/Unsubscribe.java
+++ ipc/src/main/java/com/ycl/ipc/annotation/Unsubscribe.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 反注册
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Unsubscribe {
